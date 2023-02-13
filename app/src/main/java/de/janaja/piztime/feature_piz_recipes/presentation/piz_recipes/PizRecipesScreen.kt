013a66e305635403efb8eb27f2f35538229f4ab4
jana-ja <janajansen@live.de>
2023-02-13 02:11:51 +0100

    animate pizzas rolling in on home screen

--- app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipes/PizRecipesScreen.kt
+++ app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipes/PizRecipesScreen.kt
@@ -2,17 +2,10 @@ package de.janaja.piztime.feature_piz_recipes.presentation.piz_recipes
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -24,7 +17,7 @@ fun PizRecipesScreen(
     navController: NavController,
     viewModel: PizRecipesViewModel = hiltViewModel()
 ){
-    var state = viewModel.state.value
+    val state = viewModel.state.value
 
     val scope = rememberCoroutineScope()
     Column(
@@ -34,14 +27,16 @@ fun PizRecipesScreen(
             .background(MaterialTheme.colorScheme.background),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
-        val color = MaterialTheme.colorScheme.background
-        Log.i("svg", "${color.red} ${color.green} ${color.blue}")
-        state.pizRecipes.forEach { pizRecipe ->
-            PizCard(
-                pizRecipe = pizRecipe,
-                onClick = {
-                    navController.navigate(Screen.PizRecipeDetailScreen.route + "pizRecipeId=${pizRecipe.id}")
-                })
+
+        state.pizRecipes.forEachIndexed() { index, pizRecipe ->
+                PizCard(
+                    pizRecipe = pizRecipe,
+                    onClick = {
+                        navController.navigate(Screen.PizRecipeDetailScreen.route + "pizRecipeId=${pizRecipe.id}")
+                    },
+                    index = index,
+                    modifier = Modifier.offset(x = 200.dp))
+
         }
     }
 }
--- app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipes/components/PizCard.kt
+++ app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipes/components/PizCard.kt
@@ -1,31 +1,33 @@
 package de.janaja.piztime.feature_piz_recipes.presentation.piz_recipes.components
 
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import de.janaja.piztime.R
 import de.janaja.piztime.feature_piz_recipes.domain.model.PizRecipe
 import de.janaja.piztime.ui.theme.PizTimeTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PizCard(
     pizRecipe: PizRecipe,
     onClick: () -> Unit,
+    index: Int = 0,
     modifier: Modifier = Modifier,
     cornerRadius: Dp = 10.dp
 ) {
@@ -36,19 +38,60 @@ fun PizCard(
                 .fillMaxWidth(),
         ) {
 
-            Text(text = pizRecipe.title, style = MaterialTheme.typography.titleMedium)
-            Image(
-                painter = painterResource(id = pizRecipe.imageResourceId),
-                contentDescription = "Image of ${pizRecipe.title}",//stringResource(id = ),
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.size(150.dp).align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = pizRecipe.feature,
-                style = MaterialTheme.typography.bodyMedium,
-                fontStyle = FontStyle.Italic,
-                modifier = Modifier.align(Alignment.End)
-            )
+
+            var isVisible by remember {
+                mutableStateOf(false)
+            }
+
+            val transition = updateTransition(targetState = isVisible, null)
+
+            val pizRotation by transition.animateFloat(
+                transitionSpec = {tween(1000)},
+                label = ""
+            ){
+                if (it) 360f else 0f
+            }
+            val pizOffset by transition.animateFloat(
+                transitionSpec = {tween(1000)},
+                label = ""
+            ){
+                if (it) 0f else -300f
+            }
+
+//            AnimatedVisibility(visible = isVisible) {
+                Text(text = pizRecipe.title, style = MaterialTheme.typography.titleMedium)
+//            }
+            Box(modifier = Modifier.offset(pizOffset.dp).align(Alignment.CenterHorizontally)) {
+                Image(
+                    painter = painterResource(id = pizRecipe.imageResourceId),
+                    contentDescription = "Image of ${pizRecipe.title}",//stringResource(id = ),
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .size(150.dp)
+
+                        .rotate(pizRotation)
+
+                )
+            }
+
+//            AnimatedVisibility(visible = isVisible) {
+                Text(
+                    text = pizRecipe.feature,
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier.align(Alignment.End)
+                )
+//            }
+
+            val coroutineScope = rememberCoroutineScope()
+
+            SideEffect {
+                coroutineScope.launch() {
+                    delay((index * 300).toLong())
+                    isVisible = true
+                }
+            }
+            
         }
 
     }
