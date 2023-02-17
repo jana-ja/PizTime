c4242144269c62481ba6a7caac1489b60ddf7937
JanaJa <janajansen@live.de>
2023-02-17 17:33:59 +0100

    add pizza recipe

--- app/src/main/java/de/janaja/piztime/feature_piz_recipes/data/repository/RepositoryImpl.kt
+++ app/src/main/java/de/janaja/piztime/feature_piz_recipes/data/repository/RepositoryImpl.kt
@@ -52,17 +52,18 @@ class RepositoryImpl(
             PizRecipe(
                 "Pizza Neapel",
                 "Fluffiger Rand",
-                "This recipe is for a 24 hour prove. I know it sounds like a long time but don’t worry!\n" +
-                        "Simply make the dough the night before you want to make pizza and you will be good to go on the following evening.\n" +
-                        "Don’t worry about exact timings, anywhere around 20-28 hours will be fine.\n" +
-                        "Check out my pizza dough calculator here to find the more precise amount of yeast required. This will usually be between 0.2g – 0.5g depending on the type of yeast and your room temperature.",
+                "35 35 Poolish\n" +
+                        "Let it pool for 4 hours room temperature\n" +
+                        "Then let it pool for 12-17 hours cool.\n" +
+                        "Put at room tempi 3 hours before processing",
                 R.drawable.bsp_piz
             ),
             listOf(
-                PizIngredient("Mehl", 155.0),
-                PizIngredient("Water", 95.0),
+                PizIngredient("Mehl", 149.0),
+                PizIngredient("Water", 97.0),
                 PizIngredient("Salz", 3.5),
-                PizIngredient("Trockenhefe", 0.0875)
+                PizIngredient("Trockenhefe", 0.5),
+                PizIngredient("Honig", 0.5)
             )
         ),
         Pair(
