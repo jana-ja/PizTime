53d3b806981db701d25ca35c7ae2b26896544bfd
jana-ja <janajansen@live.de>
2023-03-04 00:18:19 +0100

    move pizza animation to header view

--- app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipe_detail/PizRecipeDetailScreen.kt
+++ app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipe_detail/PizRecipeDetailScreen.kt
@@ -1,17 +1,11 @@
 package de.janaja.piztime.feature_piz_recipes.presentation.piz_recipe_detail
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -23,13 +17,11 @@ import de.janaja.piztime.feature_piz_recipes.presentation.piz_recipe_detail.comp
 import de.janaja.piztime.feature_piz_recipes.presentation.piz_recipe_detail.components.StepsView
 import de.janaja.piztime.feature_piz_recipes.presentation.piz_recipe_detail.components.IngredientsView
 import de.janaja.piztime.feature_piz_recipes.presentation.util.DummyData
-import kotlinx.coroutines.launch
 
 @Composable
 fun PizRecipeDetailScreen(
     viewModel: PizRecipeDetailViewModel = hiltViewModel()
 ) {
-    val coroutineScope = rememberCoroutineScope()
 
     val amountState = viewModel.detailAmountState.value
     val recipeState = viewModel.pizRecipeState.value
@@ -38,53 +30,15 @@ fun PizRecipeDetailScreen(
 
     //  TODO extra states for edit
 
-    // animation stuff
-    val animDuration = 1100
-    var screenVisible by remember {
-        mutableStateOf(false)
-    }
-    val transition = updateTransition(targetState = screenVisible, null)
-//    val offset by transition.animateFloat(
-//        transitionSpec = { tween(animDuration, easing = LinearOutSlowInEasing) },
-//        label = ""
-//    ) {
-//        if (it) 0f else 200f
-//    }
-
-    val pizOffset by transition.animateFloat(
-        transitionSpec = { tween(animDuration, easing = LinearOutSlowInEasing) },
-        label = ""
-    ) {
-        if (it) 0f else -400f
-    }
-    val pizRotation by transition.animateFloat(
-        transitionSpec = { tween(animDuration, easing = LinearOutSlowInEasing) },
-        label = ""
-    ) {
-        if (it)
-            0f
-        else
-            -360f
-    }
-
     PizRecipeDetailView(
         modifier = Modifier,//.offset(x = offset.dp),
         recipeState.pizRecipe,
         amountState.amount,
         { viewModel.increaseAmount() },
         { viewModel.decreaseAmount() },
-        pizOffset,
-        pizRotation
     )
 
 
-    SideEffect {
-        coroutineScope.launch {
-            screenVisible = true
-//                delay((animDuration / 2).toLong())
-//                cardVisible = true
-        }
-    }
 }
 
 @Composable
@@ -93,30 +47,27 @@ fun PizRecipeDetailView(
     pizRecipeWithDetails: PizRecipeWithDetails,
     amount: Int,
     increaseAmount: () -> Unit,
-    decreaseAmount: () -> Unit,
-    pizOffset: Float,
-    pizRotation: Float
+    decreaseAmount: () -> Unit
 ) {
 
     val overlap: Dp = dimensionResource(id = R.dimen.topSheetBorderHeight)
     val headerHeight = 150.dp
 
     Box {
-        // screen content
         Column(
             modifier = modifier
                 .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(-overlap)
+            verticalArrangement = Arrangement.spacedBy(-overlap)
 
         ) {
             // if sections should have different background colors or have no gaps in general,
             // then they have to overlap (negative space in layout arrangement and extra padding for the views tops)
 
-
             HeaderView(
                 modifier = Modifier.zIndex(1f),
                 title = pizRecipeWithDetails.title,
                 feature = pizRecipeWithDetails.feature,
+                imageResId = pizRecipeWithDetails.imageResourceId,
                 contentModifier = Modifier,
                 height = headerHeight
             )
@@ -129,7 +80,7 @@ fun PizRecipeDetailView(
                 decreaseAmount = decreaseAmount,
                 contentModifier = Modifier.padding(top = overlap),
 
-            )
+                )
 
             StepsView(
                 modifier = Modifier.zIndex(.8f),
@@ -138,29 +89,7 @@ fun PizRecipeDetailView(
                 contentModifier = Modifier.padding(top = overlap)
 
             )
-
         }
-
-        // TODO kann ich die piz jetzt doch in den header packen, weil der header nicht animiert wird?
-        // pizza image
-        Box(
-            modifier = Modifier
-                .offset(pizOffset.dp)
-                .align(Alignment.TopEnd)
-                .padding(top = 16.dp, end = 16.dp)
-        ) {
-            Image(
-                painter = painterResource(id = pizRecipeWithDetails.imageResourceId),
-                contentDescription = "Image of ${pizRecipeWithDetails.title}",//stringResource(id = ),
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .size(headerHeight / 3 * 2)
-                    .rotate(pizRotation)
-
-            )
-        }
-
-
     }
 }
 
@@ -173,9 +102,7 @@ fun PizRecipeDetailViewPreview() {
         DummyData.DummyPizRecipeWithDetails,
         4,
         { },
-        { },
-        0f,
-        0f
+        { }
     )
 }
 
--- app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipe_detail/components/HeaderView.kt
+++ app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipe_detail/components/HeaderView.kt
@@ -1,28 +1,68 @@
 package de.janaja.piztime.feature_piz_recipes.presentation.piz_recipe_detail.components
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import de.janaja.piztime.R
 import de.janaja.piztime.feature_piz_recipes.presentation.util.TopSheetShape
 import de.janaja.piztime.feature_piz_recipes.presentation.util.bottomElevation
+import kotlinx.coroutines.launch
 
 @Composable
 fun HeaderView(
     modifier: Modifier = Modifier,
     title: String,
     feature: String,
+    imageResId: Int,
     contentModifier: Modifier = Modifier,
     height: Dp
 ) {
+
+    // animation stuff
+    val animDuration = 1100
+    var screenVisible by remember {
+        mutableStateOf(false)
+    }
+    val transition = updateTransition(targetState = screenVisible, null)
+
+    val pizOffset by transition.animateFloat(
+        transitionSpec = { tween(animDuration, easing = LinearOutSlowInEasing) },
+        label = ""
+    ) {
+        if (it) 0f else -400f
+    }
+    val pizRotation by transition.animateFloat(
+        transitionSpec = { tween(animDuration, easing = LinearOutSlowInEasing) },
+        label = ""
+    ) {
+        if (it)
+            0f
+        else
+            -360f
+    }
+    val coroutineScope = rememberCoroutineScope()
+
+    SideEffect {
+        coroutineScope.launch {
+            screenVisible = true
+        }
+    }
 
 
 
@@ -37,20 +77,40 @@ fun HeaderView(
     ) {
 
 
-        Column(
-            modifier = contentModifier
-                .padding(16.dp)
-                .height(height)
-        ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.headlineLarge
-            )
-            Text(
-                text = feature,
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(top = 16.dp, start = 8.dp)
-            )
+        Box(modifier = contentModifier
+            .padding(16.dp)
+            .height(height)) {
+            Column(
+
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+                Text(
+                    text = feature,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 16.dp, start = 8.dp)
+                )
+            }
+
+            // pizza image
+            Box(
+                modifier = Modifier
+                    .offset(pizOffset.dp)
+                    .align(Alignment.TopEnd)
+                    .padding(top = 16.dp, end = 16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = imageResId),
+                    contentDescription = "Image of $title",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .size(100.dp) // TDOD set
+                        .rotate(pizRotation)
+
+                )
+            }
         }
     }
 }
@@ -58,5 +118,5 @@ fun HeaderView(
 @Preview
 @Composable
 fun HeaderViewPreview() {
-    HeaderView(title = "Pizza", feature = "ist lecker", height = 200.dp)
+    HeaderView(title = "Pizza", feature = "ist lecker", imageResId = R.drawable.bsp_piz, height = 200.dp)
 }
