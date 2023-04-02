3b78529c7b998864f28333e73cb0a31eabb40bca
jana-ja <janajansen@live.de>
2023-04-02 14:19:06 +0200

    implement nice keyboard behavior for login dialog

--- app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipes/components/LoginView.kt
+++ app/src/main/java/de/janaja/piztime/feature_piz_recipes/presentation/piz_recipes/components/LoginView.kt
@@ -1,19 +1,25 @@
 package de.janaja.piztime.feature_piz_recipes.presentation.piz_recipes.components
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import de.janaja.piztime.feature_piz_recipes.presentation.piz_recipes.PizRecipesEvent
 import de.janaja.piztime.feature_piz_recipes.presentation.piz_recipes.PizRecipesViewModel
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun LoginView(
     viewModel: PizRecipesViewModel = hiltViewModel(),
@@ -21,6 +27,7 @@ fun LoginView(
 ) {
 
     val state = viewModel.loginState.value
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Scaffold(
         topBar = {
@@ -64,18 +71,26 @@ fun LoginView(
                 onValueChange = { viewModel.onEvent(PizRecipesEvent.EmailChanged(it)) },
                 modifier = Modifier,
                 label = { Text("E-Mail") },
-                maxLines = 1
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
             )
             TextField(
                 value = state.password,
                 onValueChange = { viewModel.onEvent(PizRecipesEvent.PasswordChanged(it)) },
                 modifier = Modifier,
                 label = { Text("Passwort") },
-                maxLines = 1
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {keyboardController?.hide()}),
             )
-
-
         }
     }
-
 }
